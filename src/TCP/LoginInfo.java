0b8e918b1a290383ee48a7b43c752e026ad9d529
user <user@DESKTOP-MJDEKI7>
2021-01-01 16:55:19 +0900

    현재 로그인 정보/ 필요하신 생성자는 만들어 쓰시면 될꺼 같습니다.

---
+++ src/TCP/LoginInfo.java
@@ -0,0 +1,18 @@
+package TCP;
+
+public class LoginInfo {
+
+	String ID;
+	String Name;
+	String SEX;
+	String Loveid;
+	String First_day;
+	
+	LoginInfo(String id,String name,String sex, String loveid, String first_day) {
+		ID = id;
+		Name =name;
+		SEX = sex;
+		Loveid = loveid;
+		First_day = first_day;
+	}
+}
